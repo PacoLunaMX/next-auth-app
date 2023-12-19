e424774c1db9346edea2ac49775ff5e0a60a8b6d
PacoLunaMX <f.hinojosa9@hotmail.com>
2023-12-19 15:31:18 -0700

    add github google and credentials auth

--- app/CreateUser/page.jsx
+++ app/CreateUser/page.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import UserForm from '../(components)/UserForm'
 
 
 
 function CreateUser() {
   return (
-    <div>Only Admins!</div>
+    <div>
+      <UserForm/>
+    </div>
   )
 }
 
--- app/api/auth/[...nextauth]/options.js
+++ app/api/auth/[...nextauth]/options.js
@@ -1,5 +1,9 @@
 import GitHubProvider from "next-auth/providers/github"
 import GoogleProvider from "next-auth/providers/google"
+import CredentialsProvider from "next-auth/providers/credentials"
+import User from "@/app/(models)/User"
+import bcrypt from bcrypt
+
 
 export const options = {
     providers:[
@@ -36,7 +40,40 @@ export const options = {
             clientId: process.env.GOOGLE_ID,
             clientSecret: process.env.GOOGLE_SECRET,
             
-        })
+        }),
+        CredentialsProvider({
+            name:"Credentials",
+            credentials:{
+                email:{
+                    lable:"email:",
+                    type:"text",
+                    placeholder:"your-email"
+                },
+                pawssord:{
+                    lable:"pawssord:",
+                    type:"pawssord",
+                    placeholder:"your-pawssord"
+                },
+            },
+            async authorize(credentials){
+                try {
+                    const foundUser = await User.findOne({email: credentials.email}).lean().exect();
+                    if(foundUser){
+                        console.log("User exists")
+                        const match = await bcrypt.compare(credentials.password, foundUser.password)
+                        if(match){
+                            console.log("Good Pass")
+                            delete foundUser.password
+                            foundUser["role"] = "Unverifed Email"
+                            return foundUser
+                        }
+                    }
+                } catch (error) {
+                    console.log(error)
+                }
+                return null
+            }
+        }),
     ],
     callbacks:{
         async jwt({token, user}){
